f59063d722330939306f892995b86738da08308a
Nathan Farley <nathanfarley@Nathans-MBP.lan>
2022-06-01 17:13:29 -0400

    Completed profile container and link with firebase.

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -20,6 +20,6 @@ export default function useAuthListener() {
                 }
             });
             return () => listener();
-        }, [firebase]);
+        }, []);
         return { user };
     }
